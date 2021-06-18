5db9148314862e311c3fe1958b22676697327f21
shegoj <shegoj@yahoo.com>
2021-06-18 20:25:56 +0100

    cut up code into smaller manageable files

--- terraform101.tf
+++ terraform101.tf
@@ -14,18 +14,14 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
-data "template_file" "ec2_user_data" {
-    template ="${file("${path.module}/bootstrap.txt")}"
-}
-
-resource "aws_instance" "app_server" {
+resource "aws_instance" "web_server" {
   ami           = "ami-0ac43988dfd31ab9a"
   instance_type = "t2.micro"
   key_name      = "test1000"
   user_data = "${data.template_file.ec2_user_data.template}"
 
   tags = {
-    Name = "WebServer100"
+    Name = "WebServer300"
   }
 }
 
